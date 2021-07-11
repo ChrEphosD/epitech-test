13cd87d032412feb31f3cfa59c101a77eb75f379
Christophe DAVID <david.christophe75@gmail.com>
2021-07-11 22:48:22 +0200

    test commit

--- star.py
+++ star.py
@@ -1,6 +1,6 @@
 def star():
   suit = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25]
-  #suite qui va servir pour la branche du haut
+  #suite qui va servir pour la branche du haut.
 
   u = float(input("Entrez un chiffre entier svp"))
   while u.is_integer() != True:
